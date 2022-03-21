a27a449e0b685b4bd0cf76db2cbfff343e35f107
omarwright07 <omarwright07@gmail.com>
2022-03-21 18:18:01 -0500

    set up the generator to display anything

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
 // Assignment code here
 
+// Prompt for the length of the password min and a max
+
+// Ask and add character types to include in the password
+
+// Ask whether or not to include lowercase, uppercase, mumeric, and/or special characters
+
+// add a confirm/validate for the criteria prompts
+
+var specialCharacters = "terst";
+var password = "test";
+
+var generatePassword = function () {
+  password = "lsjlkfdjslkfjl";
+  return password;
+};
+
+
+
+var characterLimit = function () {
+  
+  
+};
+
+// tools for displaying info
+// window.prompt("test");
+// window.alert("test");
+
+// tools enforce character limit
+Math.random;
+Math.max;
+Math.floor;
+
+// loop
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
