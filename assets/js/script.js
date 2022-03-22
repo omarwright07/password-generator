9e75574b62f64208916eef9ea9196e8c1613b87e
omarwright07 <omarwright07@gmail.com>
2022-03-21 20:15:51 -0500

    made the basic criteria prompt and started the function to confirm value type of character limit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,81 @@
 // Assignment code here
+var passwordCriteria = {
 
-// Prompt for the length of the password min and a max
+}
+var confirmCharacterLimit = false;
+var confirmSpecialCharacters = false;
+var confirmUpperCase = false;
+var confirmLowerCase = false;
 
-// Ask and add character types to include in the password
+var specialCharacters = "test";
+// specialCharacters.length;
 
-// Ask whether or not to include lowercase, uppercase, mumeric, and/or special characters
 
-// add a confirm/validate for the criteria prompts
 
-var specialCharacters = "terst";
-var password = "test";
 
+// Function ---------------------------------------
+// Ask the user for parameters for the function generate Password
+var setCharacterLimit = function () {
+  var confirmCharacterLimit = window.prompt("What's your desired character limit? [ Length of at least 8 characters and no more than 128 characters ]");
+  if (typeof confirmCharacterLimit === "number" 
+  && confirmCharacterLimit > 8 
+  && confirmCharacterLimit < 128) {
+    return confirmCharacterLimit    
+  } else {
+    window.alert("Please enter a number value between 8 and 128")
+    confirmCharacterLimit();
+  }
+};
+
+
+// Tools for displaying info:
+// window.prompt("test");
+// window.alert("test");
+// window.confirm("test");
+
+// Function ---------------------------------------
+// Ask the user for parameters for the function generate Password
+var promptCriteria = function () {
+  var promptConfirmPassword = window.confirm("Would you like to generate a password?");
+  if (promptConfirmPassword) {
+    // Need a function to confirm if the Character Limit Input is a number
+    // confirmCharacterLimit = window.prompt("What's your desired character limit? [ Length of at least 8 characters and no more than 128 characters ]");
+    confirmLowerCase = window.confirm("Do you want Lower Case?");
+    confirmUpperCase = window.confirm("Do you want Upper Case?");
+    confirmNum = window.confirm("Do you want Numbers?");
+    confirmSpecialCharacters = window.confirm("Do you want Special Characters?");
+    window.alert("Criteria for password set! Generating password now!");
+  } else {
+    window.alert("No problem! Maybe next time.");
+  }
+} 
+
+// Function ---------------------------------------
+// Generates the actual password based on parameters generated from the promptCriteria function
+// Used by the writePassword function
 var generatePassword = function () {
-  password = "lsjlkfdjslkfjl";
+  //promptCriteria();
+  password = "test" + "this";
   return password;
 };
 
+for ()
 
 
-var characterLimit = function () {
-  
-  
-};
-
-// tools for displaying info
-// window.prompt("test");
-// window.alert("test");
 
 // tools enforce character limit
 Math.random;
 Math.max;
 Math.floor;
+Math.floor(Math.random());
 
-// loop
+// loop to generate password
+password = password + 
 
 
+
+// ####################################################
+// ####################################################
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -50,3 +91,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// ####################################################
+// ####################################################
