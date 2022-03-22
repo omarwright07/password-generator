50622ec175d841ef47edd2171c3383fc6061f058
omarwright07 <omarwright07@gmail.com>
2022-03-22 00:24:05 -0500

    mod the character limit to accept any input but reject anything not a number

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,55 @@
 var passwordCriteria = {
 
 }
+var promptConfirmPassword = false;
 var confirmCharacterLimit = 8;
-var confirmSpecialCharacters = false;
-var confirmUpperCase = false;
-var confirmLowerCase = false;
+var confirmSpecialCharacters = true;
+var confirmUpperCase = true;
+var confirmLowerCase = true;
+
+
 
 // Object that stores every type of character
-characters = {
-  letters: "abcdefghijklmnopqrstuvwxyz",
+var characters = {
+  letter: "abcdefghijklmnopqrstuvwxyz",
   number: "0123456789",
   special: " !#$%&()*+,-./:;<=>?@^_`{|}~",
-}
-
-// Function ---------------------------------------
-// Below is used to create random variables
-var randomNum = function (min,max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-var randomCharacter = function (cake) {
-  str = cake.charAt(randomNum(0,cake.length));
-  return str;
-}
-
-// variable.charAt();
-
+};
 
 // Tools to enforce character limit:
 // Math.random;
 // Math.max;
 // Math.floor;
-// Math.floor(Math.random());
+// [NEW] variable.charAt();
+// [NEW] Number();
+// [NEW] variable.length
+
+// Function ---------------------------------------
+// Below is used to create random variables
+var randomNum = function (min,max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+var randomCharacter = function (cake) {
+  str = cake.charAt(randomNum(0,cake.length));
+  return str;
+};
 
 // Function ---------------------------------------
 // Ask the user for parameters for the function generate Password
 var setCharacterLimit = function () {
-  var confirmCharacterLimit = window.prompt("What's your desired character limit? [ Length of at least 8 characters and no more than 128 characters ]");
-  if (typeof confirmCharacterLimit === "number" 
-  && confirmCharacterLimit > 8 
-  && confirmCharacterLimit < 128) {
-    return confirmCharacterLimit    
+  confirmCharacterLimit = window.prompt("What's your desired character limit? [ Length of at least 8 characters and no more than 128 characters ]");
+  confirmCharacterLimit = Number(confirmCharacterLimit);
+  if (typeof confirmCharacterLimit === typeof 0 
+  && confirmCharacterLimit >= 8 
+  && confirmCharacterLimit <= 128) {
+    window.alert("You set the character limit to: " + confirmCharacterLimit);
+    // return confirmCharacterLimit; 
   } else {
-    window.alert("Please enter a number value between 8 and 128")
-    confirmCharacterLimit();
+    window.alert("Please enter a number value between 8 and 128");
+    setCharacterLimit();
   }
 };
-
 
 // Tools for displaying info:
 // window.prompt("test");
@@ -57,72 +60,63 @@ var setCharacterLimit = function () {
 // Function ---------------------------------------
 // Ask the user for parameters for the function generate Password
 var promptCriteria = function () {
-  var promptConfirmPassword = window.confirm("Would you like to generate a password?");
+  promptConfirmPassword = window.confirm("Would you like to generate a password?");
   if (promptConfirmPassword) {
-    // Need a function to confirm if the Character Limit Input is a number
-    // confirmCharacterLimit = window.prompt("What's your desired character limit? [ Length of at least 8 characters and no more than 128 characters ]");
+    // #### Need a function to confirm if the Character Limit Input is a number ####
+    setCharacterLimit();
     confirmLowerCase = window.confirm("Do you want Lower Case?");
     confirmUpperCase = window.confirm("Do you want Upper Case?");
     confirmNum = window.confirm("Do you want Numbers?");
     confirmSpecialCharacters = window.confirm("Do you want Special Characters?");
     window.alert("Criteria for password set! Generating password now!");
   } else {
-    window.alert("No problem! Maybe next time.");
   }
-} 
-
-// Function ---------------------------------------
-// Generates the actual password based on parameters generated from the promptCriteria function
-// Used by the writePassword function
-var generatePassword = function () {
-  //promptCriteria();
-  password = "";
-  while (password.length < confirmCharacterLimit) {
-    switch (randomNum(0,3)) {
-      // LowerCase
-      case 0:
-        if (confirmLowerCase) {
-          password = password + "0";
-          window.alert(password);
-        } else {
-        }
-        break;
-      // UpperCase
-      case 1:
-        password = password + "1";
-        window.alert(password);
-        break;
-      // Number
-      case 2:
-        password = password + "2";
-        window.alert(password);
-        break;
-      // SpecialCharacter
-      case 3:
-        password = password + "3";
-        window.alert(password);
-        break;
-      default:
-        password = password + "n";
-        window.alert(password);
-        break;
-    }
-  }
-  window.alert(password);
-  return password;
 };
 
-// for (){
-//   password = "";
-//   password = password + "0";
-//   ret
-// }
-
+// Function ---------------------------------------
+// Generates the actual password based on parameters generated from the promptCriteria functionand used by the writePassword function
+var generatePassword = function () {
+  promptCriteria();
+  if (promptConfirmPassword) {
+    password = "";
+    while (password.length < confirmCharacterLimit) {
+      switch (randomNum(0,3)) {
+        // LowerCase
+        case 0:
+          password = password + "0";
+          window.alert(password);
+          break;
+        // UpperCase
+        case 1:
+          password = password + "1";
+          window.alert(password);
+          break;
+        // Number
+        case 2:
+          password = password + "2";
+          window.alert(password);
+          break;
+        // SpecialCharacter
+        case 3:
+          password = password + "3";
+          window.alert(password);
+          break;
+        // #### Just in case the number isn't 0, 1, 2,or 3 ####
+        default:
+          password = password + "n";
+          window.alert(password);
+          break;
+      }
+    }
+    window.alert("Final password is: " + password);
+    return password;
+  } else {
+    window.alert("No problem! Maybe next time.");
+  }
+};
 
 // loop to generate password
 // password = password + 
-
-
 
 // ####################################################
 // ####################################################
@@ -137,7 +131,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
